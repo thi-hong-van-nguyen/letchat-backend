3c79507d20d274327c30adaf78d5dc8c7ddb7041
thi <vannguyen141290@gmail.com>
2022-01-20 01:01:13 -0800

    wip

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+router.get('/', async (req, res, next) => {
+	try {
+		const users = await User.find();
+		res.status(200).json(users);
+	} catch {
+		(err) => next(err);
+	}
+});
+
 router.put('/:id/profilePic', async (req, res, next) => {
 	try {
 		const { id } = req.params;
