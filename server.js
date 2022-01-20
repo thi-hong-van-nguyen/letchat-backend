b648679dfc32eb577dfc4b07f768eaca85597e40
thi <vannguyen141290@gmail.com>
2022-01-20 00:15:23 -0800

    wip

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(cors());
 server.use(helmet());
 
 mongoose.connect(
-	'mongodb+srv://thinguyen:1f9oGorIycfotWOC@cluster0.aiph4.mongodb.net/letchat?retryWrites=true&w=majority',
+	process.env.MONGO_URL,
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
