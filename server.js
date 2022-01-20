74200d0ebcfee9a74479133263015cf6a230d8dd
thi <vannguyen141290@gmail.com>
2022-01-20 00:28:46 -0800

    wip

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
+server.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	next();
+});
+
 mongoose.connect(
 	`${process.env.MONGO_URI}`,
 	{
